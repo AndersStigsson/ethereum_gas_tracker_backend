0e34117efbebdcbc3e59b72a790565911259f1c5
Kim Kaarlenkaski <kim.kaarlenkaski@gmail.com>
2021-01-10 15:54:08 +0100

    add cronjob for hour average

--- cron/cron.sh
+++ cron/cron.sh
@@ -2,7 +2,6 @@
 cd /ethereum_gas_tracker_backend
 config=config/providers.json
 getgas=./cron/getgas.sh
-getgasavg=./cron/average1m.sh
 providers=$(jq -r 'keys[]' $config)
 
 for provider in $providers
@@ -12,4 +11,8 @@ for provider in $providers
 
 # wait 5 seconds for all providers to respond
 sleep 5
-$getgasavg $config > data/average1m.json &
+./cron/average1m.sh $config > data/average1m.json &
+./cron/average1h.sh > data/average1h.json
+
+# clean up log
+echo $(tail -n 180 data/average1m.log) > data/average1m.log
